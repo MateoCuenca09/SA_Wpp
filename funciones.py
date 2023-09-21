124f0af40e8659db75f7529638f28ca69040a4f5
MateoCuenca09 <cuencamatu@gmail.com>
2023-09-21 11:18:46 -0300

    doc funciones

--- funciones.py
+++ funciones.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def filtro_ascesor(df):
+    """  
+    Funcion que detecta en el Dataframe cuando un chat sale del menu del Bot
+    """
     try:        
         Idchats = df['ID Chat'].unique()
         idchats_no = []
@@ -15,6 +18,10 @@ def filtro_ascesor(df):
 
 
 def link_multimedia(df):
+    """  
+    Funcion que detecta cuando hay un link que lleva a un archivo multimedia 
+    y lo mueve a la columna Mensaje que no es eliminada al preprocesar.
+    """
     try:
         # Verificar si hay valores en la columna "Attachment"
         df['Mensaje'] = df.apply(lambda row: row['Attachment'] if pd.notnull(row['Attachment']) else row['Mensaje'], axis=1)
@@ -27,6 +34,9 @@ def link_multimedia(df):
     return df   
 
 def cambiar_remitente(df):
+    """  
+    Funcion que detecta cuando los mensajes enviados por el Bot y cambia la columna Remitentes
+    """
     try:
         remitentes_inbound = df[df['Tipo'] == 'INBOUND']['Remitente'].unique()
         for inbound in remitentes_inbound:
@@ -36,6 +46,9 @@ def cambiar_remitente(df):
         print("Error cambiar_remitente(): ",str(e))
 
 def obtener_rango_fechas(df):
+    """  
+    Funcion que obtiene el rango de fechas del archivo para el guardado del mismos
+    """
     try:
         df['Fecha Hora'] = pd.to_datetime(df['Fecha Hora'])
         fecha_mas_antigua = df['Fecha Hora'].dt.date.min().strftime("%Y-%m-%d")
