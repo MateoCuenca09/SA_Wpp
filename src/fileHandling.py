01e3c33dc96412174d9bad43d9c6a1d4117b1a68
Mateo Cuenca <cuencamatu@gmail.com>
2024-01-22 16:42:41 -0300

    listo para server

--- src/fileHandling.py
+++ src/fileHandling.py
@@ -14,78 +14,49 @@ folder_temp_path = 'O:/Gestion y Experiencia del Cliente/5. SERVICIO DE ATENCIÓ
 
 def guardar(df):
     try:
-        guardar_archivo(df,activo_path)
-        separar_dos_meses()
-        separar_por_mes()
+        df_existente = pd.read_csv(activo_path)
+        df_nuevo = pd.concat([df, df_existente], ignore_index=True)
+        df_nuevo = df_nuevo.drop_duplicates(ignore_index=True, keep='last', subset='ID Mensaje')
+    except FileNotFoundError:
+        print("No se encuentra archivo Historico")
+        df_nuevo = df
+
+    try:
+        df_nuevo.loc[:, 'Fecha Hora'] = pd.to_datetime(df_nuevo['Fecha Hora']) # Convierto mi columna a datetime
+        df_nuevo.loc[:, 'Mes'] = df_nuevo['Fecha Hora'].dt.strftime('%m-%Y') # Formateo mes-año
+
+        df_nuevo.sort_values(by='Fecha Hora', ascending=False, inplace=True) # Ordeno por fecha mas actual
+
+        df_actual = df_nuevo.iloc[:20000] # Separo las ultimas lineas
+        df_actual.to_csv(activo_path, index = False, encoding='utf-8-sig') # Guardo las ultimas lineas
+
+        df_antiguo = df_nuevo.iloc[20000:] # Selecciono el resto de lineas
+        separar_por_mes(df_antiguo) # Separo y guardo x mes
+        df_nuevo.to_csv(activo_path, index = False, encoding='utf-8-sig')
+
     except Exception as e:
         print("Error guardar(): ",e)
 
-def separar_dos_meses():
-    try:        
-        df = pd.read_excel(activo_path)
-        df['Fecha_Hora_temp'] = pd.to_datetime(df['Fecha Hora'])
-        fecha_reciente = pd.to_datetime(df['Fecha Hora'].max())
-        delta_tiempo = 60 # Tiempo en dias que se quiere acumular en el Excel historico
-        fecha_antigua = fecha_reciente - pd.to_timedelta(delta_tiempo, unit='D')
+def separar_por_mes(df_antiguo):
+    try:
+        
+        grupos_por_mes = df_antiguo.groupby('Mes') # Agrupo x mes
 
-        df_activo = df.loc[df['Fecha_Hora_temp']>= fecha_antigua]
-        df_dump = df.loc[df['Fecha_Hora_temp']<= fecha_antigua]
-        guardar_archivo(df_dump,temp_path)
-        df_activo.to_excel(activo_path, index = False)
-
+        # Iterar sobre cada grupo y guardar en archivos CSV
+        for mes, grupo in grupos_por_mes:
+            nombre_archivo = f'{mes}.csv'  # Establecer el nombre del archivo
+            ruta = folder_temp_path + nombre_archivo
+            grupo.to_csv(ruta, index=False, encoding='utf-8-sig')
 
     except Exception as e:
-        print("Error separar_dos_meses(): ",e)
+        print("Error separar_por_mes(): ", e)
 
 
-def guardar_archivo(df,path):
+def move_file(archivo):
     try:
-        df_existente = pd.read_excel(path)
-        df_nuevo = pd.concat([df, df_existente], ignore_index=True)
-        df_nuevo = df_nuevo.drop_duplicates(ignore_index=True, keep='last', subset='ID Mensaje')
-        df_nuevo.to_excel(path, index = False)
-    except FileNotFoundError:
-        print("Creando nuevo archivo, ",path)
-        df.to_excel(path, index = False)
-    except Exception as e:
-        print("Error al guardar_archivo()")
-        print("Archivo: ",path)
-        print("Error: ",e)
-
-
-
-def separar_por_mes():
-    try:
-        df_dump = pd.read_excel(temp_path)
-        df_dump['Fecha_Hora_temp'] = df_dump['Fecha Hora']
-        df_dump['Fecha_Hora_temp'] = pd.to_datetime(df_dump['Fecha_Hora_temp'])
-        df_dump['Fecha_Hora_temp'] = df_dump['Fecha_Hora_temp'].dt.strftime('%Y-%m')
-        meses = df_dump['Fecha_Hora_temp'].unique()
-        for mes in meses:
-            df_mes = df_dump.loc[df_dump['Fecha_Hora_temp'] == mes]
-            df_mes = df_mes.drop('Fecha_Hora_temp', axis=1)
-            archivo_path = folder_temp_path + mes + ".xlsx"
-            guardar_archivo(df_mes, archivo_path)
-
-    except Exception as e:
-        print("Error separar_por_mes(): ", e) 
-
-def move_file(archivo_a_mover):
-    try:
-        shutil.move(archivo_a_mover, carpeta_analizados)
+        archivo_completo = carpeta + archivo
+        shutil.copy2(archivo_completo, carpeta_analizados)
+        os.remove(archivo_completo)
         print("Archivo movido con EXITO!")
-
-    except FileExistsError:
-        try:
-            print("Se encontro una copia del archivo: ",archivo_a_mover)
-            os.remove(archivo_a_mover)
-            print("Archivo Eliminado")
-        except Exception as e:
-            print("Se encontro una copia del archivo: ",archivo_a_mover)
-            print("Error al intentar eliminarlo: ",e)
     except Exception as e:
-        print("1er Error move_file(): ", e)
-        try:
-            shutil.move("Copia" + archivo_a_mover, carpeta_analizados)
-        except Exception as e2:
-            print("2do Error move_file(): ", e2)
+        print(e)
